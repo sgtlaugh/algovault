d04d21936a7ddbcbb1b154b9624e39549010175f
Sabit Zahin <sgtlaugh@gmail.com>
2020-04-04 03:44:38 +0600

    Calculate fibonacci numbers with fast doubling algorithm

---
+++ code_library/python3/fast_fibonacci.py
@@ -0,0 +1,42 @@
+def fibonacci(n):
+    """
+    calculates the nth fibonacci number using fast doubling algorithm
+    fib(0) = 0, fib(1) = 1 and fib(n) = fib(n-1) + fib(n-2) for n > 1
+
+    :param n:
+    :return: nth fibonacci number
+    """
+
+    def _fib(n):
+        if n == 0:
+            return 0, 1
+
+        c, d = _fib(n >> 1)
+        a, b, z = c, d, (d << 1) - c
+        x, y = a * z, a * a + b * b
+
+        if n & 1:
+            x, y = y, x + y
+
+        return x, y
+
+    x, y = _fib(n)
+    return x
+
+
+def main():
+    print(fibonacci(0))     # 0
+    print(fibonacci(1))     # 1
+    print(fibonacci(2))     # 1
+    print(fibonacci(10))    # 55
+    print(fibonacci(100))   # 354224848179261915075
+    print(fibonacci(400))   # 176023680645013966468226945392411250770384383304492191886725992896575345044216019675
+
+    # the following works pretty fast too, ~1s locally
+    print(fibonacci(10000))
+    print(fibonacci(100000))
+    print(fibonacci(1000000))
+
+
+if __name__ == '__main__':
+    main()
