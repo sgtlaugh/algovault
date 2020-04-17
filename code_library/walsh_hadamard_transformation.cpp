fbc6c9bde14e4feb6b20ea2eee50ef2fc7827991
Sabit Zahin <sgtlaugh@gmail.com>
2020-04-17 15:24:45 +0600

    Remove unused defines

--- code_library/walsh_hadamard_transformation.cpp
+++ code_library/walsh_hadamard_transformation.cpp
@@ -2,10 +2,6 @@
 #include <bits/stdtr1c++.h>
 
 #define MAX 1048576
-#define clr(ar) memset(ar, 0, sizeof(ar))
-#define read() freopen("lol.txt", "r", stdin)
-#define dbg(x) cout << #x << " = " << x << endl
-#define ran(a, b) ((((rand() << 15) ^ rand()) % ((b) - (a) + 1)) + (a))
 
 using namespace std;
 
