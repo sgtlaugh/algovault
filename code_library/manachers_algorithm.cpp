4e8faa1b471ec3c0df218d2936969974a2fb0db8
Sabit Zahin <sgtlaugh@gmail.com>
2020-04-21 13:56:20 +0600

    Add Manacher's algorithm to find longest palindromic centers

---
+++ code_library/manachers_algorithm.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <bits/stdtr1c++.h>
+
+using namespace std;
+
+/***
+ *
+ * Manacher's algorithm to generate longest palindromic substrings for all centers
+ * For a string of length n, returns a vector of size 2 * n - 1
+ * When i is even, pal[i] = largest palindromic substring centered from str[i / 2]
+ * When i is odd,  pal[i] = largest palindromic substring centered between str[i / 2] and str[i / 2] + 1
+ *
+***/
+
+vector <int> manacher(char *str){
+    int i, j, k, l = strlen(str), n = l << 1;
+    vector <int> pal(n);
+
+    for (i = 0, j = 0, k = 0; i < n; j = max(0, j - k), i += k){
+        while (j <= i && (i + j + 1) < n && str[(i - j) >> 1] == str[(i + j + 1) >> 1]) j++;
+        for (k = 1, pal[i] = j; k <= i && k <= pal[i] && (pal[i] - k) != pal[i - k]; k++){
+            pal[i + k] = min(pal[i - k], pal[i] - k);
+        }
+    }
+
+    pal.pop_back();
+    return pal;
+}
+
+int main(){
+    char str[] = "abababbaa";
+
+    auto v = manacher(str);
+    for (auto x: v) printf("%d ", x);  /// 1 0 3 0 5 0 5 0 3 0 1 4 1 0 1 2 1
+    puts("");
+
+    return 0;
+}
