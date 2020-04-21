5ae5add50bb1ab37aca50e6fd36910f3ae26e109
Sabit Zahin <sgtlaugh@gmail.com>
2020-04-21 14:01:20 +0600

    Refactor Z Algorithm

--- code_library/python3/z_algorithm.py
+++ code_library/python3/z_algorithm.py
@@ -12,8 +12,12 @@ def z_function(text):
     l, r = 0, 0
     for i in range(1, n):
         z = max(0, min(z_array[i - l], r - i))
-        while (i + z) < n and text[z] == text[i + z]: z += 1
-        if (i + z) > r: l, r = i, i + z
+        while (i + z) < n and text[z] == text[i + z]:
+            z += 1
+
+        if (i + z) > r:
+            l, r = i, i + z
+
         z_array[i] = z
 
     z_array[0] = n
