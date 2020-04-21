f6efc3facd5be15c24460f5b1a69123573a085cd
Sabit Zahin <sgtlaugh@gmail.com>
2020-04-21 14:15:58 +0600

    Refactor Miller Rabin

--- code_library/python3/miller_rabin.py
+++ code_library/python3/miller_rabin.py
@@ -1,40 +1,38 @@
 from random import randint
 
 
-def miller_rabin(n, trials=20):
-    def _is_composite(a, d, n, s):
-        if pow(a, d, n) == 1:
-            return False
-
-        for i in range(s):
-            if pow(a, 2 ** i * d, n) == n - 1:
-                return False
-
-        return True
+def miller_rabin(n, ntrials=15):
+    _SMALL_PRIMES = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
 
     if n < 2:
         return False
 
-    small_primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
-    if any(n % p == 0 for p in small_primes):
-        return n in small_primes
+    if any(n % p == 0 for p in _SMALL_PRIMES):
+        return n in _SMALL_PRIMES
 
-    x = str(bin(n - 1))
-    s = len(x) - len(x.rstrip('0'))
+    s = str(bin(n - 1))
+    s = len(s) - len(s.rstrip('0'))
+
+    witness_list = {randint(2, n - 1) for _ in range(ntrials)}
+    return not any(_is_composite(a, n, s) for a in witness_list)
+
+
+def _is_composite(a, n, s):
     d = (n - 1) >> s
+    if pow(a, d, n) == 1:
+        return False
 
-    witness_list = {randint(2, n - 1) for _ in range(trials)}
-    return not any(_is_composite(a, d, n, s) for a in witness_list)
+    return all(pow(a, (1 << i) * d, n) != (n - 1) for i in range(s))
 
 
 def main():
-    print(miller_rabin(2))                                                           # True
-    print(miller_rabin(3))                                                           # True
-    print(miller_rabin(4))                                                           # False
-    print(miller_rabin(97))                                                          # True
-    print(miller_rabin(1000000007))                                                  # True
-    print(miller_rabin(2147483647))                                                  # True
-    print(miller_rabin(1000000007 * 2147483647))                                     # False
+    print(miller_rabin(2))  # True
+    print(miller_rabin(3))  # True
+    print(miller_rabin(4))  # False
+    print(miller_rabin(97))  # True
+    print(miller_rabin(1000000007))  # True
+    print(miller_rabin(2147483647))  # True
+    print(miller_rabin(1000000007 * 2147483647))  # False
     print(miller_rabin(4547337172376300111955330758342147474062293202868155909489))  # True
 
 
