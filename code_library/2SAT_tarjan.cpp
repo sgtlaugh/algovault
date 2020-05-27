70a7ea99bfe244f22945640d2ad4e91424507935
Sabit Zahin <sgtlaugh@gmail.com>
2020-05-28 02:29:24 +0600

    Refactor 2SAT Tarjan

--- code_library/2SAT_tarjan.cpp
+++ code_library/2SAT_tarjan.cpp
@@ -1,40 +1,42 @@
+/***
+ *
+ * 2SAT with Tarjan's algorithm (1 based index for variables)
+ * Each variable can have two possible values: true or false
+ * Variables must satisfy a system of constraints on pairs of variables
+ * Positive variables are identified from 1 to n, while negative variables from -1 to -n
+ *
+ * Complexity: O(n + m), n = number of nodes and m = number of edges or constraints
+ *
+***/
+
+
+
 #include <stdio.h>
 #include <bits/stdtr1c++.h>
 
-#define MAXN 100010
 #define MAXM 10000010
 
 using namespace std;
-
 
 struct Edge{
     int v, nxt;
 } E[MAXM];
 
-/***
- *
- * 2 SAT (1 based index for variables)
- * each variable can have two possible values - true or false
- * variables must satisfy a system of constraints on pairs of variables
- * positive variables are identified from 1 to n, while negative variables from -1 to -n
- *
-***/
 
-struct TwoSat{
-    bool visited[MAXN * 2];
-    int n, m, top, stk[MAXN * 2], head[MAXN * 2];
+struct Graph{
+    int n, m, top, lim;
+    vector <int> stk, head, visited;
 
-    TwoSat(int total_nodes){
-        m = 0, top = 0;
-        n = total_nodes * 2;
-        memset(head, -1, sizeof(head));
+    Graph(int n) : n(2 * n) {
+        m = 0, lim = this->n + 2;
+        head.resize(lim, -1), visited.resize(lim, 0), stk.resize(lim, 0);
     }
 
     inline void add_edge(int u, int v){
         E[m].v = v, E[m].nxt = head[u], head[u] = m++;
     }
 
-    /// add implication, if a then b
+    /// Add implication, if a then b
     inline void add_implication(int a, int b){
         assert(abs(a) != abs(b));
 
@@ -74,16 +76,14 @@ struct TwoSat{
 
     /// returns true if the system is 2-satisfiable and false otherwise
     bool is_satisfiable(){
-        memset(visited, 0, sizeof(visited));
+        for (int i = 0; i < lim; i++) visited[i] = 0;
 
         for (int i = 2; i <= n; i += 2){
             if (!visited[i] && !visited[i ^ 1]) {
                 top = 0;
                 if (!dfs(i)){
-                    while (top) visited[stk[--top]] = false;
-                    if (!dfs(i ^ 1)) {
-                        return false;
-                    }
+                    while (top) visited[stk[--top]] = 0;
+                    if (!dfs(i ^ 1)) return false;
                 }
             }
         }
@@ -98,7 +98,7 @@ struct TwoSat{
 
     vector <int> get_assignment(){
         vector <int> set_nodes;
-        for (int i = 2; i <= n * 2; i += 2){
+        for (int i = 2; i <= n; i += 2){
             if (visited[i]) set_nodes.push_back(i >> 1);
         }
 
@@ -108,27 +108,22 @@ struct TwoSat{
 
 
 int main(){
-    const int n = 5;
-    auto sat = TwoSat(n);
+    auto g = Graph(5);
+    assert(g.is_satisfiable());
 
-    printf("%d\n\n", sat.is_satisfiable()); /// True
+    g.add_implication(-1, -2);  /// if 1 is false then 2 is false
+    g.add_implication(-2, -3);  /// if 2 is false then 3 is false
+    g.add_implication(-3, 4);   /// if 3 is false then 4 is true
+    g.add_implication(-3, -5);  /// if 3 is false then 5 is false
+    g.add_xor(1, 4);            /// exactly one of 1 or 4 must be true
+    g.add_or(4, 5);             /// either of 4 or 5 must be true
 
-    sat.add_implication(-1, -2);            /// if 1 is false then 2 is false
-    sat.add_implication(-2, -3);            /// if 2 is false then 3 is false
-    sat.add_implication(-3, 4);             /// if 3 is false then 4 is true
-    sat.add_implication(-3, -5);            /// if 3 is false then 5 is false
-    sat.add_xor(1, 4);                      /// exactly one of 1 or 4 must be true
-    sat.add_or(4, 5);                       /// either of 4 or 5 must be true
+    assert(g.is_satisfiable());
+    auto set_nodes = g.get_assignment();
+    vector <int> expected_set_nodes{1, 2, 3, 5};
+    assert(set_nodes == expected_set_nodes);
 
-    printf("%d\n", sat.is_satisfiable());   /// True
-    auto set_nodes = sat.get_assignment();
-    for (auto x: set_nodes){
-        printf("Node %d is set\n", x);      /// 1, 2, 3 and 5 will be true
-    }
-    puts("");
-
-    sat.add_xor(1, 5);
-
-    printf("%d\n", sat.is_satisfiable());   /// False
+    g.add_xor(1, 5);
+    assert(!g.is_satisfiable());
     return 0;
 }
