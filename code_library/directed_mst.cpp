87cdaf40d09f580e6ae3d1e7fb8737592b88abb1
Sabit Zahin <sgtlaugh@gmail.com>
2020-06-04 04:36:25 +0600

    Update directed_mst.cpp

--- code_library/directed_mst.cpp
+++ code_library/directed_mst.cpp
@@ -1,3 +1,14 @@
+/***
+ *
+ * Chu–Liu/Edmonds' algorithm FOR Directed minimum spanning tree
+ * Constructs a rooted tree of minimum total weight from the root node
+ * Returns -1 if no solution from root
+ * R based index for nodes
+ * 
+ * Complexity: O(n * m)
+ *
+***/
+
 #include <stdio.h>
 #include <bits/stdtr1c++.h>
 
@@ -10,15 +21,6 @@ struct Edge{
     Edge(int u, int v, int w) : u(u), v(v), w(w) {}
 };
 
-/***
- *
- * directed minimum spanning tree in O(n * m)
- * constructs a rooted tree of minimum total weight from the root node
- * returns -1 if no solution from root
- * 0 based index for nodes
- *
-***/
-
 long long directed_mst(int n, int root, vector <Edge> edges){
     const int INF = INT_MAX;
     for (auto edge: edges) assert(edge.w >= 0 && edge.w < INF);
@@ -26,8 +28,8 @@ long long directed_mst(int n, int root, vector <Edge> edges){
     int i, k, x, y;
     long long res = 0;
     vector <int> cost(n), parent(n), label(n), comp(n);
-
-    for (; ;){
+    
+    while(1) {
         for (i = 0; i < n; i++) cost[i] = INF;
         for (auto edge: edges){
             if (edge.u != edge.v && cost[edge.v] > edge.w){
@@ -37,7 +39,7 @@ long long directed_mst(int n, int root, vector <Edge> edges){
         }
 
         cost[root] = 0;
-        for (i = 0; i < n && cost[i] != INF; i++){};
+        for (i = 0; i < n && cost[i] != INF; i++) {};
         if (i != n) return -1;
 
         for (i = 0, k = 0; i < n; i++) res += cost[i];
@@ -62,20 +64,27 @@ long long directed_mst(int n, int root, vector <Edge> edges){
         }
         root = label[root], n = k;
     }
+
     return res;
 }
 
 int main(){
     vector <Edge> edges = {
-        Edge(0, 1, 5), Edge(1, 2, 10), Edge(1, 3, 5), Edge(3, 2, 1),
-        Edge(1, 4, 2), Edge(2, 3, 1), Edge(0, 2, 1), Edge(4, 0, 15)
+        Edge(0, 1, 5),
+        Edge(1, 2, 10),
+        Edge(1, 3, 5),
+        Edge(3, 2, 1),
+        Edge(1, 4, 2),
+        Edge(2, 3, 1),
+        Edge(0, 2, 1),
+        Edge(4, 0, 15),
     };
 
-    printf("%lld\n", directed_mst(5, 0, edges));  /// 9
-    printf("%lld\n", directed_mst(5, 1, edges));  /// 19
-    printf("%lld\n", directed_mst(5, 2, edges));  /// -1
-    printf("%lld\n", directed_mst(5, 3, edges));  /// -1
-    printf("%lld\n", directed_mst(5, 4, edges));  /// 22
+    assert(directed_mst(5, 0, edges) == 9);
+    assert(directed_mst(5, 1, edges) == 19);
+    assert(directed_mst(5, 2, edges) == -1);
+    assert(directed_mst(5, 3, edges) == -1);
+    assert(directed_mst(5, 4, edges) == 22);
 
     return 0;
 }
