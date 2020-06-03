9cddbc7c19a1c7cdc552ffda521946ea3b1718b0
Sabit Zahin <sgtlaugh@gmail.com>
2020-06-03 22:59:58 +0600

    Refactor fast LCS algorithm with bitset
     - Add reference link to algorithm
     - Add assertions
     - Refactor and rename file

--- code_library/bit_string_lcs.c
+++ code_library/bit_string_lcs.c
@@ -1,20 +1,22 @@
+/***
+ *
+ * Bit-String LCS (http://users.monash.edu/~lloyd/tildeStrings/Alignment/86.IPL.html)
+ * Calculates the longest common subsequence of two strings with bitset
+ *
+ * Complexity: O(N * M / 64)
+ *
+***/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include <stdlib.h>
+#include <assert.h>
 #include <time.h>
 
 #define MAX 50010
 
-
 bool flag[MAX];
 char A[MAX], B[MAX], S[2][MAX];
-
-
-/***
- * LCS with bitmasking
- * Complexity: O (N * M / 64)
-***/
 
 int lcs(char* A, char* B){
     int i, j, k, v, n, m, res = 0;
@@ -24,7 +26,7 @@ int lcs(char* A, char* B){
     for (n = 0; A[n]; n++) S[0][n] = A[n];
     for (m = 0; B[m]; m++) S[1][m] = B[m];
 
-    for (i = 0; (i * 64) < m; i++){
+    for (i = 0; i * 64 < m; i++){
         memset(mask, 0, sizeof(mask));
         for (k = 0; k < 64 && (i * 64 + k) < m; k++){
             mask[(int)S[1][i * 64 + k]] |= (1ULL << k);
@@ -43,17 +45,14 @@ int lcs(char* A, char* B){
 }
 
 int main(){
-    srand(666);
-
-    int i, n, m;
-    n = MAX - 10, m = MAX - 10;
-    for (i = 0; i < n; i++) A[i] = (rand() % 26) + 'a';
-    for (i = 0; i < m; i++) B[i] = (rand() % 26) + 'a';
+    int n = MAX - 10, m = MAX - 10;
+    for (int i = 0; i < n; i++) A[i] = ((long long)i * i % 26) + 'a';
+    for (int i = 0; i < m; i++) B[i] = ((long long)i * i * i % 26) + 'a';
     A[n] = B[m] = 0;
 
     clock_t start = clock();
-    printf("%d\n", lcs(A, B));  /// 16245
+    assert(lcs(A, B) == 11540);
 
-    fprintf(stderr, "\nTime taken = %0.6f\n", (clock() - start) / (1.0 * CLOCKS_PER_SEC)); /// took 0.3 s locally
+    fprintf(stderr, "Time taken = %0.6f\n", (clock() - start) / (1.0 * CLOCKS_PER_SEC)); /// 0.234000 s locally
     return 0;
 }
