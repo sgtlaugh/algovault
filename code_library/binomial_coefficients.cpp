bb9edda25bd41c972b374023ea2a15a7a710db2e
Sabit Zahin <sgtlaugh@gmail.com>
2020-06-03 22:34:58 +0600

    Refactor code and add more tests

--- code_library/binomial_coefficients.cpp
+++ code_library/binomial_coefficients.cpp
@@ -1,10 +1,19 @@
+/***
+ *
+ * Binomial co-efficients (n choose k) modulo m
+ * Let m = p1^q1 * p2^q2 * ... * pk^qk, then pi^qi must not exceed MAXP for all i in [1, k]
+ * Otherwise the function can sometimes calculate the answer if it's 0 and at other times will raise an assertion error
+ * This is a generalization of the Lucas theorem, which calculates binomial mod p^q
+ * Can pre-process dp[i] for queries if m is constant
+ *
+***/
+
 #include <stdio.h>
 #include <bits/stdtr1c++.h>
 
 #define MAXP 10000010
 
 using namespace std;
-
 
 namespace crt{
     long long extended_gcd(long long a, long long b, long long& x, long long& y){
@@ -25,14 +34,13 @@ namespace crt{
         return inv;
     }
 
-    long long chinese_remainder(vector <long long> ar, vector <long long> mods){
+    long long chinese_remainder(const vector <long long>& ar, const vector <long long>& mods){
         long long x, y, res = 0, M = 1;
-
         for (int i = 0; i < (int)ar.size(); i++) M *= mods[i];
         for (int i = 0; i < (int)ar.size(); i++){
             x = M / mods[i];
             y = mod_inverse(x, mods[i]);
-            res = (res + (((x * ar[i]) % M) * y)) % M;
+            res = (res + ((x * ar[i] % M) * y)) % M;
         }
 
         return res;
@@ -53,12 +61,16 @@ namespace bin{
     }
 
     long long expo(long long x, long long n, long long m){
-        if (!n) return 1;
-        else if (n & 1) return ((expo(x, n - 1, m) * x) % m);
-        else{
-            long long r = expo(x, n >> 1, m);
-            return ((r * r) % m);
+        x %= m;
+        long long res = 1;
+
+        while (n){
+            if (n & 1) res = res * x % m;
+            x = x * x % m;
+            n >>= 1;
         }
+
+        return res % m;
     }
 
     long long factorial(long long x, long long p){
@@ -67,7 +79,7 @@ namespace bin{
         return res * dp[x % mod] % mod;
     }
 
-    /// binomial co-efficients modulo p^q (p must be a prime)
+    /// binomial co-efficients modulo p^q (p must be a prime and p^q less than MAXP)
     long long binomial(long long n, long long k, long long p, long long q){
         if (k > n) return 0;
         if (n == k || k == 0) return 1;
@@ -77,25 +89,19 @@ namespace bin{
         long long t = trailing(n, p) - trailing(k, p) - trailing(n - k, p);
         if (t >= q) return 0;
 
-        assert(mod < MAXP);
+        if (mod >= MAXP){
+            throw std::invalid_argument("p^q is not less than MAXP");
+        }
+
         for (dp[0] = 1, i = 1; i < mod; i++){
             dp[i] = (long long)dp[i - 1] * (i % p ? i : 1) % mod;
         }
 
-        long long res = factorial(n, p) * expo(factorial(k, p) * factorial(n - k, p) % mod, (mod / p) * (p - 1) - 1, mod) % mod;
-        while (t--) res = res * p % mod;
-        return res;
-    }
+        long long m = factorial(k, p) * factorial(n - k, p) % mod;
+        long long v = factorial(n, p) * expo(m, (mod / p) * (p - 1) - 1, mod) % mod;
 
-    /***
-     *
-     * binomial co-efficients modulo m
-     * let m = p1^q1 * p2^q2 * ... * pk^qk, then pi^qi must not exceed MAXP for all i in [1, k]
-     * otherwise the function can sometimes calculate the answer if it's 0 and at other times will raise an assertion error
-     * this is a generalization of the lucas theorem, which calculates binomial mod p^q
-     * can pre-process dp[i] for queries if m is constant
-     *
-    ***/
+        return v * expo(p, t, mod) % mod;
+    }
 
     long long binomial(long long n, long long k, long long m){
         if (k > n || m == 1) return 0;
@@ -124,18 +130,46 @@ namespace bin{
 }
 
 int main(){
-    printf("%lld\n", bin::binomial(1, 1, 1));                                                 /// 0
-    printf("%lld\n", bin::binomial(10, 5, 2));                                                /// 0
-    printf("%lld\n", bin::binomial(10, 5, 253));                                              /// 252
-    printf("%lld\n", bin::binomial(123456789012345LL, 1000000009, 997 * 1000003));            /// 140000420
-    printf("%lld\n", bin::binomial(1000000000000000000LL, 1000000000, 1000003));              /// 0
-    printf("%lld\n", bin::binomial(1000000000000000000LL, 1000000000, 666666667));            /// 0
-    printf("%lld\n", bin::binomial(100000000000000000LL, 10000000009, 1000000007 * 997LL));   /// 353000002471
-    printf("%lld\n", bin::binomial(1000000000000000000LL, 10000000009, 1000000007 * 997LL));  /// 361000002527
-    printf("%lld\n", bin::binomial(1000000000000000000LL, 1000, 1000000007 * 997LL));         /// 625000004375
+    using namespace bin;
 
-    /// Uncommenting the next line should raise an assertion error
-    /// printf("%lld\n", bin::binomial(1000000000000000000LL, 1000, 1000000007 * 666666667LL));
+    int dp[30][30];
+    for (int m = 1; m < 30; m++){
+        memset(dp, 0, sizeof(dp));
 
+        for (int n = 0; n < 30; n++){
+            for (int k = 0; k <= n; k++){
+                if (k == 0 || n == k) dp[n][k] = 1 % m;
+                else dp[n][k] = (dp[n - 1][k - 1] + dp[n - 1][k]) % m;
+            }
+        }
+
+        for (int n = 0; n < 30; n++){
+            for (int k = 0; k < 30; k++){
+                assert(dp[n][k] == binomial(n, k, m));
+            }
+        }
+    }
+
+    typedef tuple <long long, long long, long long, long long> T;
+    const vector <T> test_data = {
+        T(10, 5, 253, 252),
+        T(123456789012345LL, 1000000009, 997 * 1000003, 140000420),
+        T(1000000000000000000LL, 1000000000, 1000003, 0),
+        T(1000000000000000000LL, 1000000000, 666666667, 0),
+        T(100000000000000000LL, 10000000009LL, 1000000007 * 997LL, 353000002471LL),
+        T(1000000000000000000LL, 10000000009LL, 1000000007 * 997LL, 361000002527LL),
+        T(1000000000000000000LL, 1000, 1000000007 * 997LL, 625000004375LL),
+    };
+
+    bool raised_exception = false;
+
+    try{
+        cout << binomial(1000000000000000000LL, 1000, 1000000007 * 666666667LL) << endl;
+    }
+    catch(std::invalid_argument& e){
+        raised_exception = true;
+    }
+
+    assert(raised_exception);
     return 0;
 }
