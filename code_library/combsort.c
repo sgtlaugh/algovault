2ca8d2cc35d053612f6d5a3ebb2271088ed92857
Sabit Zahin <sgtlaugh@gmail.com>
2020-06-03 23:36:24 +0600

    Fix negative numbers on rand()

--- code_library/combsort.c
+++ code_library/combsort.c
@@ -1,3 +1,5 @@
+/// https://en.wikipedia.org/wiki/Comb_sort
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -7,7 +9,6 @@
 
 int n, ar[MAX];
 
-/// https://en.wikipedia.org/wiki/Comb_sort
 void combsort(int n, int* ar){
     if (n <= 1) return;
     int i, x, g = n, flag = 0;
@@ -27,11 +28,11 @@ void combsort(int n, int* ar){
 int main(){
     int i;
     n = MAX - 10;
-    for (i = 0; i < n; i++) ar[i] = (rand() << 15 ^ rand()) % 1000000007;
+    for (i = 0; i < n; i++) ar[i] = ((unsigned int)rand() << 15 ^ rand()) % 1000000007;
 
     clock_t start = clock();
     combsort(n, ar);
-    fprintf(stderr, "Time taken = %0.5f s\n", (clock() - start) / (1.0 * CLOCKS_PER_SEC));  /// 0.232 seconds locally
+    fprintf(stderr, "Time taken = %0.5f s\n", (clock() - start) / (1.0 * CLOCKS_PER_SEC));  /// 0.11900 seconds locally
 
     for (i = 0; (i + 1) < n; i++) assert(ar[i] <= ar[i + 1]);
     return 0;
