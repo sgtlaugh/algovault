5fae4ea1b3b899bd5b7bd427a251632058ff3eac
Sabit Zahin <sgtlaugh@gmail.com>
2020-06-17 15:58:01 +0600

    Fix indentation

--- code_library/gray_codes.c
+++ code_library/gray_codes.c
@@ -3,6 +3,7 @@
  * https://en.wikipedia.org/wiki/Gray_code
  * 
  * Gray code is an ordering of the binary numeral system such that two successive values differ in only one bit
+ * For example, the sequence of Gray codes for 3-bit numbers is: 000, 001, 011, 010, 110, 111, 101, 100
  * 
 ***/
 
