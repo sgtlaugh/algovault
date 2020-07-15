85d1b88d9686c5860103c9f1a88f73446bc29bd3
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-15 23:34:48 +0600

    Use assertions instead of print

--- code_library/python3/discrete_log.py
+++ code_library/python3/discrete_log.py
@@ -52,18 +52,18 @@ def discrete_log(a, b, m):
 
 
 def main():
-    print(discrete_log(1, 1, 1))                             # None
-    print(discrete_log(2, 1, 3))                             # 0
-    print(discrete_log(2, 3, 4))                             # None
-    print(discrete_log(2, 0, 4))                             # 2
-    print(discrete_log(6, 0, 8))                             # 3
-    print(discrete_log(2, 3, 6))                             # None
-    print(discrete_log(2, 6, 10))                            # 4
-    print(discrete_log(5, 33, 58))                           # 9
-    print(discrete_log(3589, 58, 97))                        # None
-    print(discrete_log(3589, 1, 97))                         # 0
-    print(discrete_log(1000000000, 666666667, 1000000007))   # 942190576
-    print(discrete_log(97383811, 35158600862, 100000000009)) # 127906939
+    assert discrete_log(1, 1, 1) is None
+    assert discrete_log(2, 1, 3) == 0
+    assert discrete_log(2, 3, 3) is None
+    assert discrete_log(2, 0, 4) == 2
+    assert discrete_log(6, 0, 8) == 3
+    assert discrete_log(2, 3, 6) is None
+    assert discrete_log(2, 6, 10) == 4
+    assert discrete_log(5, 33, 58) == 9
+    assert discrete_log(3589, 58, 97) is None
+    assert discrete_log(3589, 1, 97) == 0
+    assert discrete_log(1000000000, 666666667, 1000000007) == 942190576
+    assert discrete_log(97383811, 35158600862, 100000000009) == 127906939
 
 
 if __name__ == '__main__':
