cee955ad8b339e93435e6851b1f76bea365060e1
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-16 19:15:23 +0600

    Refactor Lagrange polynomial interpolation

--- code_library/python3/lagrange_polynomial_interpolation.py
+++ code_library/python3/lagrange_polynomial_interpolation.py
@@ -1,13 +1,12 @@
 def lagrange(x_values, y_values, nth_term):
     """
-    If a polynomial has degree K, then it can be uniquely identified if it's values are known in K + 1 distinct points
+    If a polynomial has degree k, then it can be uniquely identified if it's values are known in k+1 distinct points
 
     :param x_values: X values of the polynomial
     :param y_values: Y values of the polynomial
     :param nth_term: nth_term of the polynomial
-    :return: evaluates and returns the nth_term of the polynomial, given at least K + 1 unique values were provided
+    :return: evaluates and returns the nth_term of the polynomial in O(k^2), given at least k+1 unique values were provided
     """
-
 
     if len(x_values) != len(y_values):
         raise ValueError('The X and Y values should be of the same length')
@@ -26,9 +25,13 @@ def lagrange(x_values, y_values, nth_term):
 
 
 def main():
-    # https://mathworld.wolfram.com/CircleDivisionbyChords.html
-    # f(x) = binomial(n, 4) + binomial(n, 2) + 1
-    #      = (n^4 - 6n^3 + 23n^2 -18n + 24) / 24
+    """
+
+    Example polyonmial - https://mathworld.wolfram.com/CircleDivisionbyChords.html
+    f(x) = binomial(n, 4) + binomial(n, 2) + 1
+    or, f(x) = (n^4 - 6n^3 + 23n^2 -18n + 24) / 24
+
+    """
 
     x_values = [0, 1, 2, 3, 4]
     y_values = [1, 1, 2, 4, 8]
