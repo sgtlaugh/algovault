0ebff7b0cb0ad26e1d52901c888fc530364d947c
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-20 01:52:43 +0600

    Use vector instead of array

--- code_library/picks_theorem.cpp
+++ code_library/picks_theorem.cpp
@@ -9,37 +9,39 @@ struct Point{
 };
 
 /// returns the area of the polygon multiplied by 2
-long long area2(int n, Point poly[]){
+long long area2(const vector<Point>& poly){
     long long res = 0;
-    for (int i = 0, j = n - 1; i < n; j = i++){
+    int i, j, n = poly.size();
+    for (i = 0, j = n - 1; i < n; j = i++){
         res += ((poly[j].x + poly[i].x) * (poly[j].y - poly[i].y));
     }
     return abs(res);
 }
 
 /// number of lattice points strictly on the polygon border (edges)
-long long on_border(int n, Point poly[]){
+long long on_border(const vector<Point>& poly){
     long long res = 0;
-    for (int i = 0, j = n - 1; i < n; j = i++){
+    int i, j, n = poly.size();
+    for (i = 0, j = n - 1; i < n; j = i++){
         res += __gcd(abs(poly[i].x - poly[j].x), abs(poly[i].y - poly[j].y));
     }
     return res;
 }
 
 /// number of lattice points strictly inside polygon
-long long on_interior(int n, Point poly[]){
-    long long res = 2 + area2(n, poly) - on_border(n, poly);
+long long on_interior(const vector<Point>& poly){
+    long long res = 2 + area2(poly) - on_border(poly);
     return res >> 1;
 }
 
 int main(){
-    Point polygon[] = {
+    auto polygon = {
         Point(0, 0), Point(10, 0), Point(10, 10), Point(5, 20), Point(0, 10)
     };
 
-    printf("%lld\n", area2(5, polygon));        /// 300
-    printf("%lld\n", on_border(5, polygon));    /// 40
-    printf("%lld\n", on_interior(5, polygon));  /// 131
+    printf("%lld\n", area2(polygon));        /// 300
+    printf("%lld\n", on_border(polygon));    /// 40
+    printf("%lld\n", on_interior(polygon));  /// 131
 
     return 0;
 }
