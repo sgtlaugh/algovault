043310f0e7a66202b8c2ae17c25c4691d43032fe
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-20 03:09:35 +0600

    Refactor and simplify code

--- code_library/maximum_divisors.cpp
+++ code_library/maximum_divisors.cpp
@@ -1,35 +1,25 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
 
+using namespace std;
 
-const int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71};
-const int p = sizeof(primes) / sizeof(int);
-
+const vector <int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71};
 
 unsigned long long n, res, idx;
 
-
-unsigned long long mul(unsigned long long a, unsigned long long b){
-    unsigned long long res = 0;
-
-    while (b){
-        if (b & 1LL) res = (res + a);
-        if (res > n) return 0;
-        a = (a << 1LL);
-        b >>= 1LL;
-    }
-
-    return res;
+unsigned long long multiply(unsigned long long a, unsigned long long b){
+    if ((long double)a * b > (n + 0.5)) return 0;
+    return a * b;
 }
 
 void backtrack(int i, int lim, unsigned long long val, unsigned long long r){
-    if ((r > res) || (r == res && val < idx)) res = r, idx = val;
-    if (i == p) return;
+    if (r > res || (r == res && val < idx)) res = r, idx = val;
+    if (i == (int)primes.size()) return;
 
     int d;
     unsigned long long x = val;
 
     for (d = 1; d <= lim; d++){
-        x = mul(x, primes[i]);
+        x = multiply(x, primes[i]);
         if (x == 0) return;
         backtrack(i + 1, d, x, r * (d + 1));
     }
