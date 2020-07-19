12b606c7705e1ee982857c886bd5a5ed92c3aa3c
Sabit Zahin <sgtlaugh@gmail.com>
2020-07-20 03:12:02 +0600

    Simplify implementation

--- code_library/maximum_divisors.cpp
+++ code_library/maximum_divisors.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 
-const vector <int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71};
-
 unsigned long long n, res, idx;
+const vector <int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71};
 
 unsigned long long multiply(unsigned long long a, unsigned long long b){
     if ((long double)a * b > (n + 0.5)) return 0;
@@ -15,10 +14,8 @@ void backtrack(int i, int lim, unsigned long long val, unsigned long long r){
     if (r > res || (r == res && val < idx)) res = r, idx = val;
     if (i == (int)primes.size()) return;
 
-    int d;
     unsigned long long x = val;
-
-    for (d = 1; d <= lim; d++){
+    for (int d = 1; d <= lim; d++){
         x = multiply(x, primes[i]);
         if (x == 0) return;
         backtrack(i + 1, d, x, r * (d + 1));
@@ -31,8 +28,7 @@ void print_smallest_num_with_max_div(long long limit) {
      * Tested for n <= 10^18
     ***/
 
-    res = 0;
-    n = limit;
+    res = 0, n = limit;
     backtrack(0, 100, 1, 1);
     printf("Smallest number is %llu with %llu divisors\n", idx, res);
 }
