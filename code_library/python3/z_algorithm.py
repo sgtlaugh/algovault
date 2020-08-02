a3a7518740bdf5cdbb26b6d38c7ad2d63e7805e8
Sabit Zahin <sgtlaugh@gmail.com>
2020-08-02 17:06:44 +0600

    Improve Z Algorithm
     - Make implementation clearer
     - Improve performance

--- code_library/python3/z_algorithm.py
+++ code_library/python3/z_algorithm.py
@@ -1,21 +1,21 @@
 def z_function(text):
     """
     Z Algorithm in O(n)
-
     :param text: text string to process
     :return: the z_array, where z_array[i] = length of the longest common prefix of text[i:] and text
     """
 
     n = len(text)
-    z_array = [0] * n
-    l, r = 0, 0
+    z_array = [0 for _ in range(n)]
+    l = r = 0
 
     for i in range(1, n):
-        z = max(0, min(z_array[i - l], r - i))
-        while i + z < n and text[z] == text[i + z]:
-            z += 1
+        z = z_array[i - l]
+        if i + z >= r:
+            z = max(r - i, 0)
+            while i + z < n and text[z] == text[i + z]:
+                z += 1
 
-        if i + z > r:
             l, r = i, i + z
 
         z_array[i] = z
