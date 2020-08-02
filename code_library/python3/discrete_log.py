1eb16bdf462767fade39cbc261bfb6f5df2644ce
Sabit Zahin <sgtlaugh@gmail.com>
2020-08-02 17:57:49 +0600

    Fix discrete log calculation
     - Fix bug
     - Remove ambiguous 0^0 case
     - Refactor
     - Add more test cases

--- code_library/python3/discrete_log.py
+++ code_library/python3/discrete_log.py
@@ -1,23 +1,21 @@
 def discrete_log(a, b, mod):
-    """returns smallest non-negative x s.t. pow(a, x, mod) == b or None if no such x exists"""
-
-    if b >= mod:
-        return None
-
-    if b == 1 or mod == 1:
-        return 0
+    """
+    returns smallest non-negative x s.t. pow(a, x, mod) == b % mod
+    or None if no such x exists
+    """
 
     mp = {}
     e, n = 1, int(mod**0.5) + 2
 
-    for j in range(1, n + 1):
-        e = e * a % mod
+    for j in range(n + 3):
         if e == b:
             return j
-        mp[b * e % mod] = j
 
-    v = e
-    for i in range(2, n + 2):
+        mp[b * e % mod] = j
+        e = e * a % mod
+
+    v = e = pow(a, n, mod)
+    for i in range(2, n + 3):
         e = e * v % mod
         if e in mp:
             x = n * i - mp[e]
@@ -25,7 +23,8 @@ def discrete_log(a, b, mod):
 
 
 def main():
-    assert discrete_log(1, 1, 1) is None
+    assert discrete_log(0, 0, 1) == 1
+    assert discrete_log(1, 1, 1) == 0
     assert discrete_log(2, 1, 3) == 0
     assert discrete_log(2, 3, 3) is None
     assert discrete_log(2, 0, 4) == 2
