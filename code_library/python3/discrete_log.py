2b57b80648373062269c59574b74bdf099bce638
Sabit Zahin <sgtlaugh@gmail.com>
2020-08-02 19:17:31 +0600

    Add note about assuming 0^0 being undefined

--- code_library/python3/discrete_log.py
+++ code_library/python3/discrete_log.py
@@ -2,6 +2,9 @@ def discrete_log(a, b, mod):
     """
     returns smallest non-negative x s.t. pow(a, x, mod) == b % mod or None if no such x exists.
     Note: works even if a, b and mod are not pairwise coprime.
+    
+    Also note that it is assumed 0^0 is undefined although Python treats 0^0 as 1 (https://en.wikipedia.org/wiki/Zero_to_the_power_of_zero)
+    If it is required otherwise, should be handled explicitly (Just adding `if b == 1 or mod == 1: return 0` should suffice)
     """
 
     mp = {}
