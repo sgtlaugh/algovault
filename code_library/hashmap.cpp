ebab315ebb7c3c51491015a8275dcd7a0c8342ad
Sabit Zahin <sgtlaugh@gmail.com>
2021-02-22 05:48:07 +0600

    Improve hash map implementation

--- code_library/hashmap.cpp
+++ code_library/hashmap.cpp
@@ -1,103 +1,155 @@
-#include <stdio.h>
-#include <bits/stdtr1c++.h>
+/***
+ * Simple open addressing hash table with linear probing
+ * Every operation is O(1) with very light constant factor
+ *
+ * Uses SM Hash for choosing the buckets randomly
+ * Need to specify the maximum number of entries to be inserted on construction
+ * This is because of better performance as keys are not redistributed
+ *
+ * Default values for keys is 0, like STL map
+ *
+***/
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-struct Hashmap{
-    int t, sz, hmod;
-    vector <int> id;
-    vector <long long> key, val;
+template <typename TKey, typename TValue>
+class HashMap{
+    int cur_id, _size, hash_mod;
 
-    inline int randint(int a, int b){
-        unsigned int x = rand(), y = rand();
-        unsigned int num = (x << 15) ^ y;
-        return num % (b - a + 1) + a;
+    vector <int> id;
+    vector <TKey> keys;
+    vector <TValue> values;
+
+    bool is_prime(int n){
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++){
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 
-    inline unsigned long long smhash(unsigned long long h){
+    int next_prime(int n){
+        while (!is_prime(++n)) {}
+        return n;
+    }
+
+    inline unsigned long long sm_hash(unsigned long long h){
         h ^= h >> 33;
         h *= 0xff51afd7ed558ccd;
         h ^= h >> 33;
         h *= 0xc4ceb9fe1a85ec53;
         h ^= h >> 33;
+
         return h;
     }
 
-    inline int next_prime(int n){
-        for (int i = n; ;i++){
-            for (int j = 2; ;j = j + 1 + (j > 2)){
-                if ((j * j) > i) return i;
-                if ((i % j) == 0) break;
-            }
-        }
-        return -1;
-    }
-
-    void clear(){t++;}
-
-    inline int pos(unsigned long long x){
-        int i = smhash(x) % hmod;
-        while (id[i] == t && (unsigned long long)key[i] != x) i++;
+    inline int get_pos(TKey x){
+        int i = sm_hash(x) % hash_mod;
+        while (id[i] == cur_id && keys[i] != x) i++;
         return i;
     }
 
-    inline void erase(long long x){
-        int i = pos(x);
-        if (id[i] == t) key[i] = 0, val[i] = 0, id[i] = 0, sz--;
+    void update(TKey x, TValue v, bool replace=false){
+        int i = get_pos(x);
+
+        if (id[i] == cur_id){
+            if (replace) values[i] = 0;
+            values[i] += v;
+            return;
+        }
+
+        keys[i] = x, values[i] = v, id[i] = cur_id;
+        _size++;
     }
 
-    inline long long find(long long x){
-        int i = pos(x);
-        return (id[i] != t) ? -1 : val[i];
-    }
+    public:
+        HashMap(int max_len){
+            max_len = max(max_len, 16);
+            mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-    inline bool contains(long long x){
-        int i = pos(x);
-        return (id[i] == t);
-    }
+            int m = max_len / 2, s = sqrt(max_len);
+            int salt = rng() % m;
+            hash_mod = next_prime(2.5 * max_len - salt);
 
-    inline void add(long long x, long long v){
-        int i = pos(x);
-        (id[i] == t) ? (val[i] += v) : (key[i] = x, val[i] = v, id[i] = t, sz++);
-    }
+            _size = 0, cur_id = 1;
+            id.resize(hash_mod + s, 0);
+            keys.resize(hash_mod + s, 0);
+            values.resize(hash_mod + s, 0);
+        }
 
-    inline int size(){
-        return sz;
-    }
+        void set(TKey x, TValue v){
+            update(x, v, true);
+        }
 
-    Hashmap(int m=32){
-        unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
-        srand(seed);
+        void add(TKey x, TValue v){
+            update(x, v, false);
+        }
 
-        m = (m << 1) - randint(1, m);
-        hmod = next_prime(max(100, m));
+        TValue get(TKey x){
+            int i = get_pos(x);
+            return id[i] == cur_id ? values[i] : 0;
+        }
 
-        sz = 0, t = 1;
-        id.resize(hmod + 0x1FF, 0);
-        key.resize(hmod + 0x1FF, 0), val.resize(hmod + 0x1FF, 0);
-    }
+        void erase(TKey x){
+            int i = get_pos(x);
+
+            if (id[i] == cur_id){
+                keys[i] = 0, values[i] = 0, id[i] = 0;
+                _size--;
+            }
+        }
+
+        bool contains(TKey x){
+            int i = get_pos(x);
+            return id[i] == cur_id;
+        }
+
+        void clear(){
+            _size = 0;
+            cur_id++;
+        }
+
+        int size(){
+            return _size;
+        }
+
 };
 
 int main(){
-    Hashmap H = Hashmap();
+    auto hashmap = HashMap<int, int>(100);
 
-    cout << H.find(5) << endl;      /// -1
-    H.add(5, 32);
-    cout << H.find(5) << endl;      /// 32
+    assert(hashmap.get(5) == 0);
 
-    H.add(5, 15);
-    cout << H.find(5) << endl;      /// 47
-    H.add(5, -47);
-    cout << H.find(5) << endl;      /// 0
+    hashmap.add(5, 10);
+    assert(hashmap.get(5) == 10);
 
-    cout << H.contains(5) << endl;  /// true
-    H.erase(5);
-    cout << H.contains(5) << endl;  /// false
+    hashmap.add(5, 5);
+    assert(hashmap.get(5) == 15);
 
-    H.add(13, 3);
-    H.add(666, 15);
-    cout << H.find(666) << endl;    /// 15
-    cout << H.size() << endl;       /// 2
+    hashmap.set(5, 32);
+    assert(hashmap.get(5) == 32);
+
+    assert(hashmap.size() == 1);
+    hashmap.erase(5);
+    assert(hashmap.size() == 0);
+
+    hashmap.erase(13);
+    assert(hashmap.size() == 0);
+
+    hashmap.set(INT_MIN, INT_MIN);
+    assert(hashmap.contains(INT_MIN));
+
+    hashmap.add(INT_MIN, 1);
+    assert(hashmap.get(INT_MIN) == (INT_MIN + 1));
+
+    hashmap.add(INT_MAX, 1);
+    assert(hashmap.contains(INT_MAX));
+
+    assert(hashmap.size() == 2);
+    hashmap.clear();
+    assert(hashmap.size() == 0);
 
     return 0;
 }
