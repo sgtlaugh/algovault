00c9624d929acaae3cfd0e2dca10636bf2acbfee
Sabit Zahin <sgtlaugh@gmail.com>
2021-04-05 01:46:45 +0600

    Improve code and make hashing faster

---
+++ code_library/hashing.cpp
@@ -0,0 +1,90 @@
+/***
+ *
+ * 64-bit hashing for vectors or strings
+ * Get the forward and reverse hash of any segment
+ * Base is chosen randomly to prevent anti-hash cases from being constructed
+ * 
+ * Complexity - O(n) to build, O(1) for each hash query
+ *
+***/
+
+#include <bits/stdc++.h>
+
+#define MAXLEN 1000010
+
+using namespace std;
+
+const uint64_t seed = chrono::system_clock::now().time_since_epoch().count();
+const uint64_t base = mt19937_64(seed)() + 1729;
+
+constexpr uint64_t mod = (1ULL << 61) - 1;
+
+int64_t base_pow[MAXLEN];
+
+uint64_t modmul(uint64_t a, uint64_t b){ /// Is not generic for any mod
+    uint64_t l1 = (uint32_t)a, h1 = a >> 32, l2 = (uint32_t)b, h2 = b >> 32;
+    uint64_t l = l1 * l2, m = l1 * h2 + l2 * h1, h = h1 * h2;
+    uint64_t ret = (l & mod) + (l >> 61) + (h << 3) + (m >> 29) + (m << 35 >> 3) + 1;
+    ret = (ret & mod) + (ret >> 61);
+    ret = (ret & mod) + (ret >> 61);
+    return ret - 1;
+}
+
+void init(){
+    base_pow[0] = 1;
+    for (int i = 1; i < MAXLEN; i++){
+        base_pow[i] = modmul(base_pow[i - 1], base);
+    }
+}
+
+struct Hasher{
+    vector<int64_t> pref, suff;
+
+    Hasher() {}
+
+    template <typename T>
+    Hasher(const vector <T>& ar){
+        if (!base_pow[0]) init();
+
+        int n = ar.size();
+        assert(n < MAXLEN);
+        pref.resize(n + 3, 0), suff.resize(n + 3, 0);
+
+        for (int i = 1; i <= n; i++){
+            pref[i] = modmul(pref[i - 1], base) + ar[i - 1] + 997;
+            if (pref[i] >= mod) pref[i] -= mod;
+        }
+
+        for (int i = n; i >= 1; i--){
+            suff[i] = modmul(suff[i + 1], base) + ar[i - 1] + 997;
+            if (suff[i] >= mod) suff[i] -= mod;
+        }
+    }
+
+    Hasher(const char* str)
+        : Hasher(vector <char>(str, str + strlen(str))) {}
+
+    uint64_t get_hash(int l, int r){
+        int64_t h = pref[r + 1] - modmul(base_pow[r - l + 1], pref[l]);
+        return h < 0 ? h + mod : h;
+    }
+
+    uint64_t rev_hash(int l, int r){
+        int64_t h = suff[l + 1] - modmul(base_pow[r - l + 1], suff[r + 2]);
+        return h < 0 ? h + mod : h;
+    }
+};
+
+int main(){
+    Hasher H = Hasher("racecar");
+
+    assert(H.get_hash(0, 6) == H.rev_hash(0, 6));
+    assert(H.get_hash(1, 5) != H.rev_hash(0, 4));
+    assert(H.get_hash(1, 1) == H.rev_hash(5, 5));
+    assert(H.get_hash(1, 1) != H.rev_hash(5, 6));
+    assert(H.get_hash(2, 4) == H.rev_hash(2, 4));
+
+    H = Hasher(vector<int> {1, 2, 3, 2, 1});
+    assert(H.get_hash(0, 4) == H.rev_hash(0, 4));
+    return 0;
+}
