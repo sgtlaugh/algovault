c0140da293ef21bdff1a541bf835fffd33cf1c5e
Sabit Zahin <sgtlaugh@gmail.com>
2021-04-08 00:19:29 +0600

    Refactor and fix bug

--- code_library/hashing.cpp
+++ code_library/hashing.cpp
@@ -14,14 +14,15 @@
 
 using namespace std;
 
-const uint64_t seed = chrono::system_clock::now().time_since_epoch().count();
-const uint64_t base = mt19937_64(seed)() + 1729;
-
 constexpr uint64_t mod = (1ULL << 61) - 1;
+
+const uint64_t seed = chrono::system_clock::now().time_since_epoch().count();
+const uint64_t base = mt19937_64(seed)() % mod + 1729;
 
 int64_t base_pow[MAXLEN];
 
-uint64_t modmul(uint64_t a, uint64_t b){ /// Is not generic for any mod
+/// Is not generic for any modulo
+uint64_t modmul(uint64_t a, uint64_t b){
     uint64_t l1 = (uint32_t)a, h1 = a >> 32, l2 = (uint32_t)b, h2 = b >> 32;
     uint64_t l = l1 * l2, m = l1 * h2 + l2 * h1, h = h1 * h2;
     uint64_t ret = (l & mod) + (l >> 61) + (h << 3) + (m >> 29) + (m << 35 >> 3) + 1;
