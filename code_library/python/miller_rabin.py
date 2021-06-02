e750e8e2aa3d31024c92f7b916474854213ef57a
Sabit Zahin <sgtlaugh@gmail.com>
2021-06-02 19:46:47 +0600

    Improve Miller Rabin primality test

--- code_library/python/miller_rabin.py
+++ code_library/python/miller_rabin.py
@@ -1,37 +1,38 @@
-from random import randint
+def is_prime(n):
+    _BASES = [2, 450775, 1795265022, 9780504, 28178, 9375, 325]
 
-
-def miller_rabin(n, ntrials=15):
-    _SMALL_PRIMES = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
-
-    if n < 2:
-        return False
-
-    if any(n % p == 0 for p in _SMALL_PRIMES):
-        return n in _SMALL_PRIMES
+    if n < 5 or n & 1 == 0 or n % 3 == 0:
+        return n == 2 or n == 3
 
     s = ((n - 1) & (1 - n)).bit_length() - 1
-    witness_list = {randint(2, n - 1) for _ in range(ntrials)}
-    return not any(_is_composite(a, n, s) for a in witness_list)
+    return not any(_is_composite(a, n, s) for a in _BASES)
 
 
 def _is_composite(a, n, s):
-    d = (n - 1) >> s
-    if pow(a, d, n) == 1:
+    p = pow(a, n >> s, n)
+    if p == 1 or p == n - 1 or a % n == 0:
         return False
 
-    return all(pow(a, (1 << i) * d, n) != (n - 1) for i in range(s))
+    for _ in range(s):
+        p = p * p % n
+        if p == n - 1:
+            return False
+
+    return True
 
 
 def main():
-    assert miller_rabin(2) is True
-    assert miller_rabin(3) is True
-    assert miller_rabin(4) is False
-    assert miller_rabin(97) is True
-    assert miller_rabin(1000000007) is True
-    assert miller_rabin(2147483647) is True
-    assert miller_rabin(1000000007 * 2147483647) is False
-    assert miller_rabin(4547337172376300111955330758342147474062293202868155909489) is True
+    assert is_prime(1) is False
+    assert is_prime(2) is True
+    assert is_prime(3) is True
+    assert is_prime(4) is False
+    assert is_prime(97) is True
+    assert is_prime(299210837) is True
+    assert is_prime(1000000007) is True
+    assert is_prime(2147483647) is True
+    assert is_prime(1000000007 * 2147483647) is False
+    assert is_prime(1000003 * 474119 * 699053) is False
+    assert is_prime(4547337172376300111955330758342147474062293202868155909489) is True
 
 
 if __name__ == '__main__':
