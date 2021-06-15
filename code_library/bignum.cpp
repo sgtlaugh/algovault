1cbff074ebda2b7fd4c94b93bae516cfa50f48fa
Sabit Zahin <sgtlaugh@gmail.com>
2021-06-16 02:37:10 +0600

    Refactor big integer class

--- code_library/bignum.cpp
+++ code_library/bignum.cpp
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <bits/stdtr1c++.h>
-
-#define SIZE 100
+#include <bits/stdc++.h>
 
 using namespace std;
 
-const int base_len = 8;
-const long long base = 0x5F5E100;
+constexpr int SIZE = 100;
+constexpr int BASE_PW = 9;
+
+constexpr long long BASE = pow(10, BASE_PW) + 0.25;
+
+char tmpbuf[SIZE * BASE_PW + 10];
 
 struct UnsignedBignum{
     int len;
@@ -19,9 +20,24 @@ struct UnsignedBignum{
         return(data[x]);
     }
 
-    UnsignedBignum(){
+    UnsignedBignum(long long x=0){
         len = 0;
         memset(data, 0, sizeof(data));
+        (*this) = x;
+    }
+
+    UnsignedBignum(const char* str){
+        int l = strlen(str + 1);
+        long long val = 0;
+        (*this).clear();
+
+        for (int i = 1; i <= (l - 1) / BASE_PW + 1; i++){
+            val = 0;
+            for (int j = l - BASE_PW * i + 1; j <= l - BASE_PW * i + BASE_PW; j++){
+                if (j >= 1) val = val * 10 + str[j] - 48;
+            }
+            data[++len] = val;
+        }
     }
 
     void clear(){
@@ -32,6 +48,7 @@ struct UnsignedBignum{
     inline int compare(const UnsignedBignum &a, const UnsignedBignum &b){
         if (a.len > b.len) return 0;
         if (a.len < b.len) return 1;
+
         for (int i = a.len; i >= 1; i--){
             if (a.data[i] > b.data[i]) return 0;
             if (a.data[i] < b.data[i]) return 1;
@@ -42,21 +59,27 @@ struct UnsignedBignum{
     inline bool operator < (const UnsignedBignum &x){
         return (compare(*this, x) == 1);
     }
+
     inline bool operator > (const UnsignedBignum &x){
         return (compare(*this, x) == 0);
     }
+
     inline bool operator <= (const UnsignedBignum &x){
         return (compare(*this, x) >= 1);
     }
+
     inline bool operator >= (const UnsignedBignum &x){
         return ((compare(*this, x) & 1) == 0);
     }
+
     inline bool operator != (const UnsignedBignum &x){
         return (compare(*this, x) != 2);
     }
+
     inline bool operator == (const UnsignedBignum &x){
         return (compare(*this, x) == 2);
     }
+
     inline UnsignedBignum operator = (const UnsignedBignum& x){
         for (int i = x.len + 1; i <= len; i++) data[i] = 0;
         for (int i = 1; i <= x.len; i++) data[i] = x.data[i];
@@ -69,8 +92,8 @@ struct UnsignedBignum{
         len = 0;
 
         do{
-            data[++len] = x % base;
-            x /= base;
+            data[++len] = x % BASE;
+            x /= BASE;
         }
         while (x);
         return *this;
@@ -79,48 +102,48 @@ struct UnsignedBignum{
     inline UnsignedBignum operator += (const UnsignedBignum &b){
         return *this = (*this + b);
     }
+
     inline UnsignedBignum operator *= (const UnsignedBignum &b){
         return *this = (*this * b);
     }
+
     inline UnsignedBignum operator -= (const UnsignedBignum &b){
         return *this = (*this - b);
     }
+
     inline UnsignedBignum operator /= (const UnsignedBignum &b){
         return *this = (*this / b);
     }
+
     inline UnsignedBignum operator %= (const UnsignedBignum &b){
         return *this = (*this % b);
     }
+
     inline UnsignedBignum operator *= (long long x){
         return (*this = (*this * x));
     }
+
     inline UnsignedBignum operator += (long long x){
         return (*this = (*this + x));
     }
+
     inline UnsignedBignum operator -= (long long x){
         return (*this = (*this - x));
     }
+
     inline UnsignedBignum operator /= (long long x){
         return (*this = (*this / x));
     }
 
-    UnsignedBignum(long long x){
-        len = 0;
-        memset(data, 0, sizeof(data));
-        (*this) = x;
-    }
-
     UnsignedBignum operator * (const UnsignedBignum& x){
-        int i, j;
-        UnsignedBignum res = UnsignedBignum(0);
-        for (i = 1; i <= len; i++){
-            if (data[i]){
-                for (j = 1; j <= x.len; j++){
-                    if (x.data[j]){
-                        res.data[i + j - 1] += data[i] * x.data[j];
-                        res.data[i + j] += res.data[i + j - 1] / base;
-                        res.data[i + j - 1] %= base;
-                    }
+        UnsignedBignum res = UnsignedBignum(0LL);
+
+        for (int i = 1; i <= len; i++){
+            for (int j = 1; data[i] && j <= x.len; j++){
+                if (x.data[j]){
+                    res.data[i + j - 1] += data[i] * x.data[j];
+                    res.data[i + j] += res.data[i + j - 1] / BASE;
+                    res.data[i + j - 1] %= BASE;
                 }
             }
         }
@@ -132,12 +155,12 @@ struct UnsignedBignum{
     }
 
     UnsignedBignum operator / (long long x){
-        assert(x != 0);
+        assert(x);
 
-        UnsignedBignum res;
         long long y = 0;
+        UnsignedBignum res;
         for (int i = len; i >= 1; i--){
-            y = y * base + data[i];
+            y = y * BASE + data[i];
             res[i] = y / x;
             y %= x;
         }
@@ -151,7 +174,7 @@ struct UnsignedBignum{
         assert(!(b.len == 1 && b[1] == 0));
 
         long long x = data[len], y = b[b.len];
-        int i, l1 = base_len * (len - 1), l2 = base_len * (b.len - 1);
+        int i, l1 = BASE_PW * (len - 1), l2 = BASE_PW * (b.len - 1);
 
         while (x) x /= 10, l1++;
         while (y) y /= 10, l2++;
@@ -159,16 +182,16 @@ struct UnsignedBignum{
         UnsignedBignum temp = b;
         rem = *this;
 
-        for (i = 1; i * base_len <= (l1 - l2); i++) temp *= base;
-        for (i = 1; i <= (l1 - l2) % base_len; i++) temp *= 10;
+        for (i = 1; i * BASE_PW <= (l1 - l2); i++) temp *= BASE;
+        for (i = 1; i <= (l1 - l2) % BASE_PW; i++) temp *= 10;
         for (i = l1 - l2; i >= 0; i--){
             x = 0;
             while (rem >= temp) rem -= temp, x++;
-            quot[i / base_len + 1] = quot[i / base_len + 1] * 10 + x;
+            quot[i / BASE_PW + 1] = quot[i / BASE_PW + 1] * 10 + x;
             temp /= 10;
         }
 
-        quot.len = (l1 - l2) / base_len + 1;
+        quot.len = (l1 - l2) / BASE_PW + 1;
         while(rem.len > 1 && !rem[rem.len]) rem.len--;
         while(quot.len > 1 && !quot[quot.len]) quot.len--;
     }
@@ -189,7 +212,7 @@ struct UnsignedBignum{
 
     long long operator % (long long x){
         long long res = 0;
-        for (int i = len; i >= 1; i--) res = (res * base + data[i]) % x;
+        for (int i = len; i >= 1; i--) res = (res * BASE + data[i]) % x;
         return res;
     }
 
@@ -197,7 +220,7 @@ struct UnsignedBignum{
         UnsignedBignum res;
         int i, l = max(len, x.len);
         for (i = 1; i <= l; i++) res[i] = data[i] + x[i];
-        for (i = 1; i <= l; i++) res[i + 1] += res[i] / base, res[i] %= base;
+        for (i = 1; i <= l; i++) res[i + 1] += res[i] / BASE, res[i] %= BASE;
 
         res.len = l;
         if (res[res.len + 1]) res.len++;
@@ -209,7 +232,7 @@ struct UnsignedBignum{
         UnsignedBignum res;
         for (int i = 1; i <= len; i++) res.data[i] = data[i] - x.data[i];
         for (int i = 1; i <= len; i++){
-            if (res[i] < 0) res.data[i] += base, res.data[i + 1]--;
+            if (res[i] < 0) res.data[i] += BASE, res.data[i + 1]--;
         }
 
         res.len = len;
@@ -217,30 +240,20 @@ struct UnsignedBignum{
         return res;
     }
 
-    UnsignedBignum(const char* str, int dummy){
-        int l = strlen(str + 1);
-        long long val = 0;
-        (*this).clear();
+    void read_bignum(){
+        assert(scanf("%s", tmpbuf + 1));
+        *this = UnsignedBignum(tmpbuf);
+    }
 
-        for (int i = 1; i <= (l - 1) / base_len + 1; i++){
-            val = 0;
-            for (int j = l - base_len * i + 1; j <= l - base_len * i + base_len; j++){
-                if (j >= 1) val = val * 10 + str[j] - 48;
-            }
-            data[++len] = val;
+    string to_str(){
+        sprintf(tmpbuf, "%lld", data[len]);
+        string s = string(tmpbuf);
+
+        for (int i = len - 1; i >= 1; i--){
+            sprintf(tmpbuf, "%0*lld", BASE_PW, data[i]);
+            s += string(tmpbuf);
         }
-    }
-
-    void nextUnsignedBignum(){
-        char str[SIZE * base_len + 10];
-        scanf("%s", str + 1);
-        UnsignedBignum(str, 666);
-    }
-
-    void print(char endChar = '\n'){
-        printf("%lld", data[len]);
-        for (int i = len - 1; i >= 1; i--) printf("%0*lld", base_len, data[i]);
-        putchar(endChar);
+        return s;
     }
 };
 
@@ -248,8 +261,7 @@ struct Bignum{
     int sign;
     UnsignedBignum val;
 
-    Bignum(){}
-    Bignum(long long x){
+    Bignum(long long x=0){
         sign = 0;
         val = llabs(x);
         if (x < 0) sign = 1;
@@ -272,7 +284,7 @@ struct Bignum{
         Bignum res;
         res.val = y * this->val;
         res.sign = this->sign ^ x.sign;
-        if (res.val == UnsignedBignum(0)) res.sign = 0;
+        if (res.val == UnsignedBignum(0LL)) res.sign = 0;
         return res;
     }
 
@@ -281,7 +293,7 @@ struct Bignum{
         Bignum res;
         res.val = y / x.val;
         res.sign = this->sign ^ x.sign;
-        if (res.val == UnsignedBignum(0)) res.sign = 0;
+        if (res.val == UnsignedBignum(0LL)) res.sign = 0;
         return res;
     }
 
@@ -290,7 +302,7 @@ struct Bignum{
         Bignum res;
         res.val = y % x.val;
         res.sign = this->sign;
-        if (res.val == UnsignedBignum(0)) res.sign = 0;
+        if (res.val == UnsignedBignum(0LL)) res.sign = 0;
         return res;
     }
 
@@ -315,7 +327,7 @@ struct Bignum{
             }
         }
 
-        if (res.val == UnsignedBignum(0)) res.sign = 0;
+        if (res.val == UnsignedBignum(0LL)) res.sign = 0;
         return res;
     }
 
@@ -339,18 +351,23 @@ struct Bignum{
     inline bool operator < (const Bignum &x){
         return (compare(*this, x) == 1);
     }
+
     inline bool operator > (const Bignum &x){
         return (compare(*this, x) == 0);
     }
+
     inline bool operator <= (const Bignum &x){
         return (compare(*this, x) >= 1);
     }
+
     inline bool operator >= (const Bignum &x){
         return ((compare(*this, x) & 1) == 0);
     }
+
     inline bool operator != (const Bignum &x){
         return (compare(*this, x) != 2);
     }
+
     inline bool operator == (const Bignum &x){
         return (compare(*this, x) == 2);
     }
@@ -358,46 +375,56 @@ struct Bignum{
     inline Bignum operator += (const Bignum &b){
         return *this = (*this + b);
     }
+
     inline Bignum operator *= (const Bignum &b){
         return *this = (*this * b);
     }
+
     inline Bignum operator -= (const Bignum &b){
         return *this = (*this - b);
     }
+
     inline Bignum operator /= (const Bignum &b){
         return *this = (*this / b);
     }
+
     inline Bignum operator %= (const Bignum &b){
         return *this = (*this % b);
     }
+
     inline Bignum operator *= (long long x){
         return (*this = (*this * x));
     }
+
     inline Bignum operator += (long long x){
         return (*this = (*this + x));
     }
+
     inline Bignum operator -= (long long x){
         return (*this = (*this - x));
     }
+
     inline Bignum operator /= (long long x){
         return (*this = (*this / x));
     }
 
-    inline void nextBignum(){
-        char str[SIZE * base_len + 10];
-        scanf("%s", str);
-        sign = (str[0] == '-');
+    inline void read_bignum(){
+        assert(scanf("%s", tmpbuf));
+        sign = (tmpbuf[0] == '-');
+
         if (!sign){
-            int i, l = strlen(str);
-            for (i = l; i >= 1; i--) str[i] = str[i - 1];
-            str[l + 1] = 0;
+            int i, l = strlen(tmpbuf);
+            for (i = l; i >= 1; i--) tmpbuf[i] = tmpbuf[i - 1];
+            tmpbuf[l + 1] = 0;
         }
-        val = UnsignedBignum(str, 666);
+
+        val = UnsignedBignum(tmpbuf);
     }
 
-    void print(char endChar = 10){
-        if (sign) putchar('-');
-        val.print(endChar);
+    string to_str(){
+        string s = "";
+        if (sign) s = s + '-';
+        return s + val.to_str();
     }
 };
 
@@ -405,24 +432,48 @@ struct Bignum{
 int main(){
     Bignum x = Bignum(1);
     for (int i = 0; i < 100; i++) x = x * Bignum(2);
-    x.print(); /// 1267650600228229401496703205376 (2^100)
+    assert(x.to_str() == "1267650600228229401496703205376");
 
     Bignum y = x + x;
-    y.print(); /// 2535301200456458802993406410752
+    assert(y.to_str() == "2535301200456458802993406410752");
 
     Bignum r = x % 1000000007;
-    r.print(); /// 976371285
+    assert(r.to_str() == "976371285");
 
     Bignum q = x / 1000000007;
-    q.print(); /// 1267650591354675262013
+    assert(q.to_str() == "1267650591354675262013");
 
     Bignum v = q * 1000000007 + r;
-    v.print(); /// 1267650600228229401496703205376
+    assert(v.to_str() == "1267650600228229401496703205376");
 
     Bignum c = v - y;
-    c.print(); /// -1267650600228229401496703205376
+    assert(c.to_str() == "-1267650600228229401496703205376");
 
     Bignum chksum = x + c;
-    chksum.print(); /// 0
+    assert(chksum.to_str() == "0");
+
+    constexpr auto is_equal = [](Bignum v1, int v2) ->bool{
+        return v1.to_str() == to_string(v2);
+    };
+
+    for (int a = -100; a < 100; a++){
+        for (int b = -100; b < 100; b++){
+            assert(is_equal(Bignum(a) + Bignum(b), a + b));
+            assert(is_equal(Bignum(a) - Bignum(b), a - b));
+            assert(is_equal(Bignum(a) * Bignum(b), a * b));
+            if (b){
+                assert(is_equal(Bignum(a) / Bignum(b), a / b));
+                assert(is_equal(Bignum(a) % Bignum(b), a % b));
+            }
+
+            assert((a < b) == (Bignum(a) < Bignum(b)));
+            assert((a > b) == (Bignum(a) > Bignum(b)));
+            assert((a == b) == (Bignum(a) == Bignum(b)));
+            assert((a != b) == (Bignum(a) != Bignum(b)));
+            assert((a >= b) == (Bignum(a) >= Bignum(b)));
+            assert((a <= b) == (Bignum(a) <= Bignum(b)));
+        }
+    }
+
     return 0;
 }
