9c4bbd08a326354cc05bece6aee9c1491f14ffda
Sabit Zahin <sgtlaugh@gmail.com>
2021-06-17 04:44:37 +0600

    Refactor bit string LCS

--- code_library/bit_string_lcs.cpp
+++ code_library/bit_string_lcs.cpp
@@ -7,52 +7,53 @@
  *
 ***/
 
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <assert.h>
-#include <time.h>
+#include <bits/stdc++.h>
 
-#define MAX 50010
+#define MAX 100000
 
-bool flag[MAX];
-char A[MAX], B[MAX], S[2][MAX];
+using namespace std;
 
-int lcs(char* A, char* B){
+int lcs(const char* A, const char* B){
+    bool flag[MAX];
     int i, j, k, v, n, m, res = 0;
     unsigned long long x, t, q, y, mask[128] = {0};
 
     memset(flag, 0, sizeof(flag));
-    for (n = 0; A[n]; n++) S[0][n] = A[n];
-    for (m = 0; B[m]; m++) S[1][m] = B[m];
+    n = strlen(A), m = strlen(B);
 
     for (i = 0; i * 64 < m; i++){
         memset(mask, 0, sizeof(mask));
-        for (k = 0; k < 64 && (i * 64 + k) < m; k++){
-            mask[(int)S[1][i * 64 + k]] |= (1ULL << k);
+        for (k = 0; k < 64 && i * 64 + k < m; k++){
+            mask[(int)B[i * 64 + k]] |= (1ULL << k);
         }
 
         for (j = 0, x = 0; j < n; j++){
-            t = mask[(int)S[0][j]] & ~x;
-            x |= t, v = flag[j];
-            q = x - (t << 1) - v, y = (q & ~x) | t;
-            flag[j] = y >> 63, x &= ~(y << 1);
-            if (v) x &= ~1ULL;
+            t = mask[(int)A[j]] & ~x;
+            x |= t;
+            v = flag[j];
+            q = x - (t << 1) - v;
+            y = (q & ~x) | t;
+            flag[j] = y >> 63;
+            x &= ~(y << 1);
+            if (v) x &= ~1;
         }
+
         res += __builtin_popcountll(x);
     }
     return res;
 }
 
 int main(){
+    char A[MAX], B[MAX];
+
     int n = MAX - 10, m = MAX - 10;
     for (int i = 0; i < n; i++) A[i] = ((long long)i * i % 26) + 'a';
     for (int i = 0; i < m; i++) B[i] = ((long long)i * i * i % 26) + 'a';
     A[n] = B[m] = 0;
 
     clock_t start = clock();
-    assert(lcs(A, B) == 11540);
+    assert(lcs(A, B) == 23075);
 
-    fprintf(stderr, "Time taken = %0.6f\n", (clock() - start) / (1.0 * CLOCKS_PER_SEC)); /// 0.234000 s locally
+    fprintf(stderr, "Time taken = %0.3f\n", (clock() - start) / (1.0 * CLOCKS_PER_SEC)); /// 0.715 s locally
     return 0;
 }
